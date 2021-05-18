8ebe5359fe5c9aade451bc429dd8e00d2aab49d5
rinamuka <rinamuka4@gmail.com>
2021-05-18 11:22:31 -0700

    Code ready

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,64 @@
-#include <header.hpp>
+#include <boost/program_options.hpp>
+#include <builder.hpp>
 
-int main() {
+#include "iostream"
+using namespace boost::program_options;
+
+int main(int argc, char **argv) {
+  std::string install_usage = "добавляем этап установки (в директорию _install)";
+  std::string pack_usage = "добавляем этап упаковки (в архив формата tar.gz))";
+
+  std::string usage = \
+ R"(Usage: builder [options]
+    Allowed options:
+  --help                        : выводим вспомогательное сообщение
+  --config <Release|Debug>      : указываем конфигурацию сборки (по умолчанию Debug)
+  --install                     : добавляем этап установки (в директорию _install)
+  --pack                        : добавляем этап упаковки (в архив формата tar.gz)
+  --timeout <count>             : указываем время ожидания (в секундах)";
+
+  options_description desc("General options");
+  std::string task_type;
+  desc.add_options()
+      ("help", usage.c_str())
+      ("config", value<std::string>()->default_value("Debug"))
+      ("install", install_usage.c_str())
+      ("pack", pack_usage.c_str())
+
+
+      ("timeout", value<int32_t>()->default_value(-1));
+  variables_map vm;
+  try {
+    parsed_options parsed = command_line_parser(argc, argv).options(desc).allow_unregistered().run();
+    store(parsed, vm);
+
+    if (vm.count("help")) {
+      std::cout << usage << std::endl;
+      return 0;
+    }
+
+    std::string config = vm["config"].as<std::string>();
+    bool install = false;
+    bool pack = false;
+    uint32_t timeout = vm["timeout"].as<int32_t>();;
+
+    if (vm.count("install"))
+      install = true;
+
+    if (vm.count("pack"))
+      pack = true;
+    try {
+      builder bldr = builder(config, install, pack, timeout);
+      bldr.startBuild();
+    }
+    catch (std::exception &ex) {
+      std::cout << ex.what() << std::endl;
+      return 1;
+    }
+  }
+  catch (std::exception &ex) {
+    std::cout << usage << std::endl;
+    return 1;
+  }
 
 }
---
+++ sources/builder.cpp
@@ -0,0 +1,78 @@
+// Copyright 2020 Your Name <your_email>
+#include <builder.hpp>
+#include "async++.h"
+namespace bp = boost::process;
+
+void builder::startBuild() {
+  if (timeout_ != -1) {
+    auto timer = async::spawn([this] {
+      std::cout << "Start build" << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(timeout_));
+      if (process_.running()) {
+        std::cout << "Time is exceed"
+                  << std::endl << "Terminating building" << std::endl;
+        process_.terminate();
+      }
+    });
+  }
+  bool success =
+      executeCommand("-H. -B_builds -DCMAKE_INSTALL_PREFIX=_install "
+              "-DCMAKE_BUILD_TYPE="
+              + config_);
+  if (!success)
+    return;
+  // build
+  success = executeCommand("--build _builds");
+  if (!success)
+    return;
+
+  if (install_ && pack_) {
+    success = executeCommand("--build _builds --target install");
+    if (!success)
+      return;
+
+    success = executeCommand("--build _builds --target package");
+    if (!success)
+      return;
+
+  } else if (install_ && !pack_) {
+    success = executeCommand("--build _builds --target install");
+    if (!success)
+      return;
+
+  } else if (!install_ && pack_) {
+    success = executeCommand("--build _builds --target package");
+    if (!success)
+      return;
+  }
+}
+
+bool builder::executeCommand( std::string arguments){
+  bp::ipstream stream;
+  auto cmake_path = boost::process::search_path("cmake");
+
+
+  std::cout << std::endl << std::endl
+            << "Executing command : " << std::endl << arguments
+            << std::endl << std::endl;
+
+  bp::child child(cmake_path.string() + " " + arguments,
+                  boost::process::std_out > stream);
+  process_ = std::move(child);
+
+  for (std::string line;
+       process_.running() && std::getline(stream, line);) {
+    std::cout << line << std::endl;
+  }
+
+  process_.wait();
+
+  auto exit_code = process_.exit_code();
+
+  if (exit_code != 0) {
+    std::cout << std::to_string(exit_code) + " exit code. Exiting..." << std::endl;
+    return false;
+  } else {
+    return true;
+  }
+}
