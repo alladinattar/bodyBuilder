a0a54545dc92fe28d4abbae7a2f07565eeb70cbc
rinamuka <rinamuka4@gmail.com>
2021-05-18 12:52:22 -0700

    fix check

--- include/builder.hpp
+++ include/builder.hpp
@@ -1,21 +1,23 @@
 // Copyright 2020 Your Name <your_email>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
+#ifndef INCLUDE_BUILDER_HPP_
+#define INCLUDE_BUILDER_HPP_
+#include <async++.h>
+#include <string>
 #include "boost/process.hpp"
 #include "iostream"
-#include <async++.h>
-class builder{
+class builder {
  private:
   std::string config_;
   bool install_;
   bool pack_;
   uint timeout_;
   boost::process::child process_;
+
  public:
-  builder(std::string config, bool install, bool pack, uint timeout):config_(config), install_(install), pack_(pack), timeout_(timeout){};
+  builder(std::string config, bool install, bool pack, uint timeout)
+      : config_(config), install_(install), pack_(pack), timeout_(timeout){};
   void startBuild();
   bool executeCommand(const std::string& arguments);
-
 };
-#endif // INCLUDE_HEADER_HPP_
+#endif  // INCLUDE_BUILDER_HPP_
